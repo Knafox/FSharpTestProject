15f6911f0ec2222eda797f7e6ac0d6e751aa7ae1
Nathan Fox <nathan.fox@markit.com>
2018-09-19 12:43:58 -0500

    Adding project filesize explorer

---
+++ Examples/Tutorial/Tutorial/Examples.fsx
@@ -0,0 +1,54 @@
+﻿
+/// F# Examples
+/// From
+
+
+namespace Examples
+open System
+
+module TailRecursion = 
+
+    /// Exampe of Factorial without Tail recursion
+    let rec Fact x = 
+        if x = 1 then
+            1
+        else
+            Fact (x-1) * x
+    
+
+    let factTail x =
+        let rec factacc acc x =
+            if x = 1 then
+                acc
+            else
+                factacc (acc*x) (x-1)
+                
+        factacc 1 x
+
+    factTail 3
+
+/// Not tail recusrive
+    let fib x =
+        if x = 0 then
+            0
+        else if x = 1 then
+            1
+        else fib (x-1) + fib (x-2) 
+
+    fib 4
+
+// Not sure how to tai resurive this functio 
+    let fibtail x =
+       1
+    
+
+
+
+
+
+#if COMPILED
+module BoilerPlateForForm = 
+    [<System.STAThread>]
+    do ()
+    do System.Windows.Forms.Application.Run()
+#endif
---
+++ FileSizeExplorer/FileSizeExplorer/Program.fs
@@ -0,0 +1,58 @@
+﻿namespace Program
+
+open System 
+open System.IO
+
+module FileParser = 
+
+//Outdated use fileINFO Type type for 
+    type File = {
+        Name: String;
+        Directory: String;
+        Extendsion: String;
+        Size: int;
+    } 
+
+//Directory and Files Pattern Match?
+
+
+
+    let GetFilesInDirectory d =
+        Directory.GetFiles d
+
+    let PrintFilesNames fileNames = 
+        for x in fileNames do
+            printfn "%s" x
+
+    let GetFileInfo name =
+        let x = FileInfo(name)
+        printfn "File: %s, is %d bytes " x.Name x.Length
+
+    let GetFilesAndSize d =
+        Directory.GetFiles d
+        |> Array.map ( fun x -> FileInfo(x) )
+        |> Array.sortBy ( fun x -> x.Length )
+        |> Array.iter ( fun x -> printfn "File: %s, is %d bytes " x.Name x.Length )
+
+
+    [<EntryPoint>]
+    let main argv = 
+        //Test 1: get the files in a directory
+        let listoffiles = GetFilesInDirectory @"D:\GotHub\FSharp\FileSizeExplorer\FileSizeExplorer\bin\Debug"
+        PrintFilesNames listoffiles
+        Console.ReadKey() |> ignore
+        //Test 2: get the size of a specific file
+        GetFileInfo @"D:\AlcentraBackUp"
+        Console.ReadKey() |> ignore
+        //Test 3: get the largest file from a list
+        GetFilesAndSize @"D:\GotHub\FSharp\FileSizeExplorer\FileSizeExplorer\bin\Debug"
+        Console.ReadKey() |> ignore
+
+        0 // return an integer exit code
+
+
+
+//    let listOfFilesInDirectory (givenDirectory:string)=
+  //      let  listOfFiles=Directory.GetFiles  givenDirectory |> List.ofSeq
+    //    listOfFiles |> List.map (fun fn -> 
+      //      Path.GetFileName(fn), FileInfo(fn).Length)
