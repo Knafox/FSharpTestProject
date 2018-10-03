00d2f01c196cf821624609aa6888dbb5a6e5e67e
Nathan Fox <knafox@gmail.com>
2018-10-03 18:38:09 -0500

    Adding exception logic

--- FileSizeExplorer/FileSizeExplorer/Program.fs
+++ FileSizeExplorer/FileSizeExplorer/Program.fs
@@ -5,32 +5,80 @@ open System.IO
 
 module FileParser = 
 
-    /// Possibly could use the given FileInfo System Type
-    type File = {
-        Name: string;
-        Directory: string;
-        Extension: string;
-        Size: double;
-    } 
+    type File = 
+        {
+            Name: string
+            Directory: string
+            Extension: string
+            Size: int64
+        }
 
+    type Directory = 
+        {
+            Path: string
+            Name: string
+            Files: seq<File>
+            TotalSize: int64
+        }
 
-//Directory and Files and extention  Pattern Match?
+    let CreateFile (x:FileInfo) = 
+        {   
+            File.Name = x.FullName
+            File.Directory = x.Directory.Name
+            File.Extension = x.Extension
+            File.Size = x.Length
+        }
+
+    let GetFileTotal x =
+        x
+        |> Seq.map (fun x -> x.Size) 
+        |> Seq.reduce (fun acc f -> acc + f)
+
+    let CreateDirectory (x:System.IO.DirectoryInfo) (y:seq<File>) =
+        {
+             Directory.Path = x.FullName
+             Directory.Name = x.Name
+             Directory.Files = y
+             Directory.TotalSize = GetFileTotal y
+        }
+
+    let TryOpenFile x =
+        try
+            FileInfo x
+            |> ignore
+            true
+        with
+        | :? System.IO.IOException -> false
+        | _ as exn -> raise exn
 
     let GetFilesInDirectory x = 
         Directory.EnumerateFiles x
-        |> Seq.map ( fun x -> FileInfo(x) )
-        |> Seq.filter ( fun x -> File.Exists(x.FullName ))
-        |> Seq.filter ( fun x -> x.Length > int64(10) )
-        |> Seq.iter ( fun x -> printfn "File: %s, of Type: %s, is %d bytes " x.Name x.Extension x.Length )
+        |> Seq.filter File.Exists
+        |> Seq.map FileInfo
+        |> Seq.map CreateFile
 
     let rec GetDirectories x =
         Directory.EnumerateDirectories x
-        |> Seq.iter( fun x -> 
+        |> Seq.iter (fun x -> 
+            let dir = DirectoryInfo x 
+            let DirList = GetFilesInDirectory x >> CreateDirectory dir 
+            printfn "%s" x
+            GetDirectories x
+        )
+
+    let GetFilesInDirectory x = 
+        Directory.EnumerateFiles x
+        |> Seq.filter TryOpenFile 
+        |> Seq.map FileInfo >> CreateFile
+        //|> Seq.iter (fun (x:File) -> printfn "File: %s, of Type: %s, is %d bytes " x.Name x.Extension x.Size)
+
+    let rec GetDirectories x =
+        Directory.EnumerateDirectories x
+        |> Seq.iter (fun x -> 
             GetFilesInDirectory x
             printfn "%s" x
-            GetDirectories x)
-
-
+            GetDirectories x
+        )
 
     [<EntryPoint>]
     let main argv =
--- FileSizeExplorer/FileSizeExplorer/Test.fsx
+++ FileSizeExplorer/FileSizeExplorer/Test.fsx
@@ -1,15 +1,23 @@
 open System 
 open System.IO
-open System.IO
 open System.Linq
 
+let TryOpenFile x =
+    try
+        FileInfo x
+        |> ignore
+        true
+    with
+    | :? System.IO.IOException -> false
+    | _ as exn -> raise exn
+
 type File = 
-    {
+   {
         Name: string
         Directory: string
         Extension: string
         Size: int64
-    }
+   }
 
 let CreateFile (x:FileInfo) = 
     {   
@@ -19,43 +27,23 @@ let CreateFile (x:FileInfo) =
         File.Size = x.Length
     }
 
-let CheckPermissions x = 
-    let dirAccess = Directory.GetAccessControl(x, Security.AccessControl.AccessControlSections.Access)
-    let indentity = @"INFOCORP\nathan.fox"
-    [for rule in dirAccess.GetAccessRules(true, true, typeof<System.Security.Principal.NTAccount>) -> rule] ///workaround
-    |> List.tryFind (fun rule -> rule.IdentityReference.Value = indentity)
-    |> function // |> fun s -> match s with ....
-        | None -> false
-        | Some _ -> true
-
-
-
-let result = CheckPermissions @"D:\GotHub\FSharp\Examples\Tutorial\Tutorial\Tutorial.fsx"
-CheckPermissions @"C:\Windows\System32\Calc.exe"        
-
-let TryOpenFile x =
-    try 
-        FileInfo >> CreateFile
-    with
-    | :? System.IO.IOException -> ignore
-    | _ as exn ->
-        raise exn 
-
 let GetFilesInDirectory x = 
     Directory.EnumerateFiles x
-    |> Seq.filter File.Exists //try to open file and wrap in try catch. true if success and false if failed.
-    |> Seq.map TryOpenFile 
-    |> Seq.iter (fun x -> printfn "File: %s, of Type: %s, is %d bytes " x.Name x.Extension x.Size)
+    |> Seq.filter File.Exists
+    |> Seq.map FileInfo
+    |> Seq.map CreateFile
 
 let rec GetDirectories x =
     Directory.EnumerateDirectories x
     |> Seq.iter (fun x -> 
+        let dir = DirectoryInfo x 
         GetFilesInDirectory x
-        //printfn "%s" x
+        |> CreateDirectory dir 
+        printfn "%s" x
         GetDirectories x
     )
 
-GetDirectories @"C:\Users\nathan.fox\AppData\Local\Application Data"
+GetDirectories @"C:\"
 
 
 //Fix Exception Issue
