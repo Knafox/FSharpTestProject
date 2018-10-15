d40aa26fe0a87cb221157e563c596a5b69318955
Nathan Fox <nathan.fox@markit.com>
2018-10-15 13:39:49 -0500

    Adding new functions for stats

--- FileSizeExplorer/FileSizeExplorer/Program.fs
+++ FileSizeExplorer/FileSizeExplorer/Program.fs
@@ -6,12 +6,12 @@ open System.IO
 module FileParser = 
 
     type File = 
-        {
+       {
             Name: string
             Directory: string
             Extension: string
             Size: int64
-        }
+       }
 
     type Directory = 
         {
@@ -19,6 +19,7 @@ module FileParser =
             Name: string
             Files: seq<File>
             TotalSize: int64
+            SubDirectory: seq<Directory>
         }
 
     let CreateFile (x:FileInfo) = 
@@ -30,60 +31,101 @@ module FileParser =
         }
 
     let GetFileTotal x =
-        x
-        |> Seq.map (fun x -> x.Size) 
-        |> Seq.reduce (fun acc f -> acc + f)
+        try
+        (
+            x
+            |> Seq.map (fun x -> x.Size) 
+            |> Seq.reduce (fun acc f -> acc + f)
+        )
+        with
+        | _ -> int64(0)
 
-    let CreateDirectory (x:System.IO.DirectoryInfo) (y:seq<File>) =
+    let CreateDirectory (x:System.IO.DirectoryInfo) (y:seq<File>) (z:seq<Directory>) =
         {
              Directory.Path = x.FullName
              Directory.Name = x.Name
              Directory.Files = y
              Directory.TotalSize = GetFileTotal y
+             Directory.SubDirectory = z
         }
 
     let TryOpenFile x =
         try
             FileInfo x
             |> ignore
-            true
+            File.Exists x
         with
         | :? System.IO.IOException -> false
         | _ as exn -> raise exn
 
     let GetFilesInDirectory x = 
         Directory.EnumerateFiles x
-        |> Seq.filter File.Exists
+        |> Seq.filter TryOpenFile
         |> Seq.map FileInfo
         |> Seq.map CreateFile
 
+    
     let rec GetDirectories x =
         Directory.EnumerateDirectories x
-        |> Seq.iter (fun x -> 
-            let dir = DirectoryInfo x 
-            let DirList = GetFilesInDirectory x >> CreateDirectory dir 
-            printfn "%s" x
-            GetDirectories x
-        )
+        |> Seq.map (fun x -> CreateDirectory (DirectoryInfo x) (GetFilesInDirectory x) (GetDirectories x))
+    
+    let printFiles (files:seq<File>) =
+        Seq.iter (fun (x:File) -> 
+            printfn "File Name: %s, Directory: %s, Extention: %s, Size: %d" x.Name x.Directory x.Extension x.Size
+        ) files
 
-    let GetFilesInDirectory x = 
-        Directory.EnumerateFiles x
-        |> Seq.filter TryOpenFile 
-        |> Seq.map FileInfo >> CreateFile
-        //|> Seq.iter (fun (x:File) -> printfn "File: %s, of Type: %s, is %d bytes " x.Name x.Extension x.Size)
+    let rec printDirectory (x:seq<Directory>) =
+        Seq.iter (fun y ->
+            printfn "Driectory Name: %s, Path: %s, TotalSize: %d" y.Name y.Path y.TotalSize
+            printFiles y.Files
+            printDirectory y.SubDirectory
+        ) x
+    
+    let rec FileCount (x:seq<Directory>) =
+        Seq.fold(fun dirAcc dir -> 
+            dirAcc + (Seq.fold(fun acc file -> acc + 1) 0 dir.Files) + FileCount dir.SubDirectory            
+        ) 0 x
+    
+    let rec DirectoryCount (x:seq<Directory>) = 
+        Seq.fold(fun dirAcc dir -> 
+            dirAcc + DirectoryCount dir.SubDirectory + 1           
+        ) 0 x
 
-    let rec GetDirectories x =
-        Directory.EnumerateDirectories x
-        |> Seq.iter (fun x -> 
-            GetFilesInDirectory x
-            printfn "%s" x
-            GetDirectories x
-        )
+    let rec Totalsize (x:seq<Directory>) =
+        Seq.fold (fun dirAcc dir -> 
+            dirAcc + dir.TotalSize + Totalsize dir.SubDirectory
+        ) (int64(0)) x
+
+    let rec Totalsize2 (x:seq<Directory>) =
+        Seq.fold (fun dirAcc dir -> 
+            dirAcc + Totalsize2 dir.SubDirectory +
+            (Seq.fold(fun acc file -> acc + file.Size) 0L dir.Files) 
+        ) (int64(0)) x
+
+    let GetAllTypes (x:seq<Directory>) (types:list<String>) =
+        Seq.iter (fun dir ->
+            
+        ) x
 
     [<EntryPoint>]
     let main argv =
-        GetDirectories @"D:\Gothub\"
+        // get the files in a directory
+        let y = GetFilesInDirectory @"C:\"
+        printFiles y 
+        //Console.ReadKey() |> ignore
+        printfn ""
 
+        //get all the files in a directory and sub directories
+        let x = GetDirectories @"D:\GotHub"
+        printDirectory x
+        //Console.ReadKey() |> ignore
+        printfn ""
+
+        //get stats on files processed
+        printfn "Total count of files is: %d! " (FileCount x)
+        printfn "Total count of directories is: %d! " (DirectoryCount x)
+        printfn "Total size of files processed is: %d! " (Totalsize x)
+        Console.ReadKey() |> ignore
         0 // return an integer exit code
 
 
@@ -92,3 +134,24 @@ module FileParser =
   //      let  listOfFiles=Directory.GetFiles  givenDirectory |> List.ofSeq
     //    listOfFiles |> List.map (fun fn -> 
       //      Path.GetFileName(fn), FileInfo(fn).Length)
+
+
+      
+//let rec GetDirectories x =
+  //  Directory.EnumerateDirectories x
+    //|> Seq.map (fun x -> CreateDirectory (DirectoryInfo x) (GetFilesInDirectory x) (GetDirectories x))
+
+
+//let printFile (x:File) = 
+  //  printfn "File Name: %s, Directory: %s, Extention: %s, Size: %d" x.Name x.Directory x.Extension x.Size
+
+//let rec printDirectory (x:Directory) =
+  //  printfn "Driectory Name: %s, Path: %s, TotalSize: %d" x.Name x.Path x.TotalSize
+    //x.Files
+    //|> Seq.iter printFile
+//    x.SubDirectory
+  //  |> Seq.iter printDirectory
+
+//let y = GetDirectories @"C:\"
+//y
+//|> Seq.iter printDirectory
--- FileSizeExplorer/FileSizeExplorer/Test.fsx
+++ FileSizeExplorer/FileSizeExplorer/Test.fsx
@@ -2,51 +2,13 @@ open System
 open System.IO
 open System.Linq
 
-let TryOpenFile x =
-    try
-        FileInfo x
-        |> ignore
-        true
-    with
-    | :? System.IO.IOException -> false
-    | _ as exn -> raise exn
 
-type File = 
-   {
-        Name: string
-        Directory: string
-        Extension: string
-        Size: int64
-   }
+let SeqCount (x:seq<int>) = 
+    Seq.fold( fun acc x -> acc + 1 ) 0 x
 
-let CreateFile (x:FileInfo) = 
-    {   
-        File.Name = x.FullName
-        File.Directory = x.Directory.Name
-        File.Extension = x.Extension
-        File.Size = x.Length
-    }
+let SeqSeqCount (x:seq<seq<int>>) =
+    Seq.fold(fun dirAcc dir -> dirAcc + Seq.fold(fun acc file -> acc + 1) 0 dir ) 0 x
 
-let GetFilesInDirectory x = 
-    Directory.EnumerateFiles x
-    |> Seq.filter File.Exists
-    |> Seq.map FileInfo
-    |> Seq.map CreateFile
+let seqseq = { 0 .. 100}
 
-let rec GetDirectories x =
-    Directory.EnumerateDirectories x
-    |> Seq.iter (fun x -> 
-        let dir = DirectoryInfo x 
-        GetFilesInDirectory x
-        |> CreateDirectory dir 
-        printfn "%s" x
-        GetDirectories x
-    )
-
-GetDirectories @"C:\"
-
-
-//Fix Exception Issue
-//Get Total size of directory and %s
-
-
+let x = SeqCount firstseq
